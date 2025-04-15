2a6c237ccb1f8739322c693042aed412e3042fae
spider660 <spiderspiders049@gmail.com>
2025-04-15 22:19:32 +0300

    Update bbr.sh

--- ubuntu/bbr.sh
+++ ubuntu/bbr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edition : Stable Edition V3.0
-# Author  : xenon tech
+# Author  : 𝐒𝐏𝐈𝐃𝐄𝐑 tech
 # (C) Copyright 2024
 # =========================================
 
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing 𝐓𝐂𝐏 𝐁𝐁𝐑 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 XENON 𝐒𝐓𝐎𝐑𝐄"
+echo -e "Installing 𝐓𝐂𝐏 𝐁𝐁𝐑 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐒𝐏𝐈𝐃𝐄𝐑 𝐒𝐓𝐎𝐑𝐄"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
